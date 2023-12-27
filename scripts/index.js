6f67d5919cb602fb2ed3a7ac844d50d66613ef41
SiegBoss <146117384+SiegBoss@users.noreply.github.com>
2023-12-27 15:45:49 -0600

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,17 @@
-var modal1 = document.getElementById("my-modal-1");
-var img1 = document.getElementById("image-modal-1");
-var modalImg1 = document.getElementById("image-1");
-
-var modal2 = document.getElementById("my-modal-2");
-var img2 = document.getElementById("image-modal-2");
-var modalImg2 = document.getElementById("image-2");
-
-var span1 = document.getElementsByClassName("close")[0];
-var span2 = document.getElementsByClassName("close")[1];
-
-
-img1.onclick = function () {
-    modal1.style.display = "block";
-    modalImg1.src = this.src;
+function openModal(imageUrl) {
+    document.getElementById('modalImage').src = imageUrl;
+    document.getElementById('modal').style.display = 'flex';
+    document.addEventListener('keydown', closeModalOnEscape);
 }
 
-img2.onclick = function () {
-    modal2.style.display = "block";
-    modalImg2.src = this.src;
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.removeEventListener('keydown', closeModalOnEscape);
 }
 
-
-span1.onclick = function () {
-    modal1.style.display = "none";
-  
-}
-
-span2.onclick = function () {
-    modal2.style.display = "none";
-  
+function closeModalOnEscape(event) {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
 }
 
