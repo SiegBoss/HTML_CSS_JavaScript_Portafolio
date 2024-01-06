a7a66de288b74277dc979f09f23ab8bb10a0486a
SiegBoss <146117384+SiegBoss@users.noreply.github.com>
2024-01-05 19:37:46 -0600

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,26 @@
+// habre el modal con la imagen que se le pase por parametro
 function openModal(imageUrl) {
+
     document.getElementById('modalImage').src = imageUrl;
     document.getElementById('modal').style.display = 'flex';
     document.addEventListener('keydown', closeModalOnEscape);
+
 }
 
+// cierra el modal
 function closeModal() {
+
     document.getElementById('modal').style.display = 'none';
     document.removeEventListener('keydown', closeModalOnEscape);
+
 }
 
+// cierra el modal si se presiona la tecla escape
 function closeModalOnEscape(event) {
+
     if (event.key === 'Escape') {
         closeModal();
     }
+    
 }
 
